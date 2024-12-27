d81cadc8c640a46490c6463fe6a56579adde4e94
Bakey <klou999d@gmail.com>
2024-12-27 14:02:44 +0300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import HomePage from "@/components/home";
-import SliderSection from "@/components/slider_section";
 
 export default function Home() {
   
-  return <SliderSection></SliderSection>
+  return <HomePage></HomePage>
 }
